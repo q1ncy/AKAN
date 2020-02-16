cd57cf0b055e25d2db104bde928a42c51d45463a
q1ncy <qbarnabas@gmail.com>
2020-02-16 14:02:16 +0300

    MOds made to Akan Generator page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-function Press(){
-    var Year=parseInt(document.getElementById("Year").value)
-    var CC=Year.slice(0,1)
-    var YY=Year.slice(2,3)
+function Akan(){
     var DD=parseInt(document.getElementById("day").value)
-    var MM=parseInt(document.getElementById("month").value)
-    var dayOftheWeek=parseInt( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7
-    alert(dayOftheWeek)
+    var MM=parseInt(document.getElementById("Month").value)
+    var CC=parseInt(document.getElementById("CC").value)
+    var YY=parseInt(document.getElementById("YY").value)
+    var DayOfTheweek=((((CC/4)-2*CC-1)+(5*YY/4)+(26*(MM+1)/10)+DD)%7).toFixed()
+     var gen=document.getElementById("Gender").value
+    if (DD<=0 || DD>31){
+        alert("Enter valid date!!!")
+    }else if (MM<=0 || MM>12) {
+        alert("Enter valid Month!!!")
+    }
 }
